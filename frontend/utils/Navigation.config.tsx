7e66edc1f764ebed9a5ad56853e8eb0b764d1ee8
Naglis Kazlauskas <naglis98@gmail.com>
2023-05-30 19:31:42 +0100

    feat(utils)

---
+++ frontend/utils/Navigation.config.tsx
@@ -0,0 +1,10 @@
+import { DocumentDuplicateIcon } from "@heroicons/react/24/outline";
+
+export const navigation = [
+	{
+		name: "Youtube Summaries",
+		href: "youtube-summaries",
+		icon: DocumentDuplicateIcon,
+		current: true,
+	},
+];
